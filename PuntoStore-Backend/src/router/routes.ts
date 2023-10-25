bb8342afe4d40591bf0425e03631c500fd6b8f8d
Sanchez Cerdan Joaquín Andrés <jac.sanchez@etec.local>
2023-10-25 12:53:37 -0300

    Punto 1 y 2 hechos

--- PuntoStore-Backend/src/router/routes.ts
+++ PuntoStore-Backend/src/router/routes.ts
@@ -1,9 +1,37 @@
 import express from 'express';
+const mainRouter = express.Router();
+const producto_y_mercancia = [
+    {
+        nombre: "Campera",
+        modelo: "Nike",
+        precio: 100,
+        pais_de_origen: "Estados Unidos"
+    },
+    {
+        nombre: "Pantalon",
+        modelo: "Adidas",
+        precio: 50,
+        pais_de_origen: "Estados Unidos"
+    },
+    {
+        nombre: "Reloj",
+        modelo: "Rolex",
+        precio: 150,
+        pais_de_origen: "Inglaterra"
+    }
+];
 
-const productsRouter = express.Router();
+mainRouter.get('/productos', (_, res) => {
+    res.send(producto_y_mercancia);
+});
 
-productsRouter.get('/', (_, res)=>{
+mainRouter.get('/precio', (_,res) =>{
+    const mayor100 = producto_y_mercancia.filter(producto => producto.precio > 99);
+    res.send(mayor100)
+})
+
+mainRouter.get('/', (_, res) => {
     res.send('Messi idolo');
 })
 
-export {productsRouter};
+export { mainRouter };
--- PuntoStore-Backend/src/server.ts
+++ PuntoStore-Backend/src/server.ts
@@ -1,9 +1,9 @@
 import express from 'express';
-import { productsRouter } from './router/routes';
+import { mainRouter } from './router/routes';
 const app = express();
 const port = 8080;
 
-app.use ('/' , productsRouter)
+app.use ('/' , mainRouter)
 
 app.listen(port, () => {
   console.log(`Servidor: http://localhost:${port}`);
