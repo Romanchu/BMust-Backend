deeff351058302eec41968b8047c9f600c3a4226
JoaSNZ <jac.sanchez@alumno.etec.um.edu.ar>
2023-10-27 12:49:12 -0300

    Puntos 1-2-4-5-6 hechos

--- PuntoStore-Backend/src/router/routes.ts
+++ PuntoStore-Backend/src/router/routes.ts
@@ -1,5 +1,10 @@
 import express from 'express';
 const mainRouter = express.Router();
+
+mainRouter.get('/', (_, res) => {
+    res.send('Punto 1: /productos ; Punto 2: /precio ; Punto 4: /eliminar/eliminarModelo; Punto 5: /pais/paisFiltrado ; Punto 6: /precio/precioFiltrado');
+});
+
 const producto_y_mercancia = [
     {
         nombre: "Campera",
@@ -21,17 +26,80 @@ const producto_y_mercancia = [
     }
 ];
 
+//1//
+
 mainRouter.get('/productos', (_, res) => {
     res.send(producto_y_mercancia);
 });
 
+//2//
+
 mainRouter.get('/precio', (_,res) =>{
     const mayor100 = producto_y_mercancia.filter(producto => producto.precio > 99);
     res.send(mayor100)
-})
+});
 
-mainRouter.get('/', (_, res) => {
-    res.send('Messi idolo');
-})
+//3//
+
+mainRouter.post('modificar/:modeloAModificar', (req, res) => {
+    const { modeloAModificar } = req.body.modelo;
+
+    const nuevoProducto = req.body.producto; 
+
+    const indiceAModificar = producto_y_mercancia.findIndex(producto => producto.modelo === String(modeloAModificar));
+    
+    if (indiceAModificar === -1) {
+        res.status(404).send('No se encontró un producto con el modelo especificado.');
+    } else {
+        producto_y_mercancia[indiceAModificar] = nuevoProducto;
+        res.send('Producto modificado con éxito.');
+    }
+});
+
+//4//
+
+mainRouter.get('/eliminar/:eliminarModelo', (req, res) => {
+    const { eliminarModelo } = req.params;
+
+    const indiceParaEliminar = producto_y_mercancia.findIndex(producto => producto.modelo === eliminarModelo);
+    
+    if (indiceParaEliminar === -1) {
+        res.status(404).send('No se encontró un producto con el modelo especificado.');
+    } else {
+        producto_y_mercancia.splice(indiceParaEliminar, 1);
+        res.send('Producto eliminado con éxito.');
+    }
+});
+
+//5//
+
+mainRouter.get('/pais/:paisFiltrado' , (req , res) => {
+    const { paisFiltrado } = req.params;
+
+    const productosPorPais = producto_y_mercancia.filter(producto => producto.pais_de_origen === paisFiltrado);
+    
+    if (productosPorPais.length === 0) {
+        res.status(404).send('No se encontraron productos para el país especificado.');
+    } else {
+        res.send(productosPorPais);
+    }
+});
+
+//6//
+
+mainRouter.get('/precio/:precioFiltrado', (req, res) => {
+    const {precioFiltrado} = req.params;
+
+    const productosPorPrecio = producto_y_mercancia.filter(producto => producto.precio === parseInt(precioFiltrado));
+    
+    if (productosPorPrecio.length === 0) {
+        res.status(404).send('No se encontraron productos para el precio especificado.');
+    } else {
+        res.send(productosPorPrecio);
+    }
+});
+
+//7//
+
 
 export { mainRouter };
